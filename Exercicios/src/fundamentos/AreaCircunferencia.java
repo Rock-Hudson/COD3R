70203bad8f94a88f9612b75f69939dc4e3188741
Rock Hudson <rockhudsonmillas@gmail.com>
2024-08-20 10:40:20 -0300

    Constante

--- Exercicios/src/fundamentos/AreaCircunferencia.java
+++ Exercicios/src/fundamentos/AreaCircunferencia.java
@@ -6,7 +6,7 @@ public class AreaCircunferencia {
 	public static void main(String[] args) {
 		
 		double raio = 3.4;
-		final double pi = 3.14159;
+		final double PI = 3.14159;
 		
 		double area = pi * (raio * raio);
 		
