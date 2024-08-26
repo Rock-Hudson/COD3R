def60502b285646ced88bd1427c01a7d95763bdc
Rock Hudson <rockhudsonmillas@gmail.com>
2024-08-25 21:35:45 -0300

    Desafios

---
+++ Desafios De Lógica/src/logica/AreaTriangulo.java
@@ -0,0 +1,30 @@
+package logica;
+
+import java.util.Scanner;
+
+public class AreaTriangulo {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+		System.out.println("Para calcular a area de um triangulo, insira o valor de base e altura:");
+		
+		System.out.println("Insira o valor de Base: ");
+		double inputBase = input.nextInt();
+		
+		System.out.println("Insira o valor de altura:");
+		double inputAltura = input.nextInt();
+		
+		
+		double area = (inputBase * inputAltura)/2;
+		
+		System.out.println("O valor da Area é:" + area);
+		
+		
+	}
+	
+	
+	
+}
