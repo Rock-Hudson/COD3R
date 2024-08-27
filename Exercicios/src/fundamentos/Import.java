ff7a2f2478f01dff793c30c1f51003c1672cba11
Rock Hudson <rockhudsonmillas@gmail.com>
2024-08-27 10:56:39 -0300

    Tipo String

---
+++ Exercicios/src/fundamentos/Import.java
@@ -0,0 +1,24 @@
+package fundamentos;
+import java.util.Date;
+
+import javax.swing.JButton;
+
+public class Import {
+
+	
+	public static void main(String[] args) {
+		
+		Date d = new Date();
+		System.out.println(d);
+		
+		JButton botao = new JButton();
+		
+	}
+	
+   
+	
+	
+
+	
+	
+}
---
+++ Exercicios/src/fundamentos/TipoString.java
@@ -0,0 +1,17 @@
+package fundamentos;
+
+public class TipoString {
+
+	public static void main(String[] args) {
+		
+		String nome = "Rock ";
+		
+		System.out.println("Meu nome é Rock".charAt(7)); // Traz a letra do indice apontado
+		System.out.println(nome.concat("Hudson"));
+		System.out.println(nome.startsWith("Hudson".lo));
+		
+		
+	}
+	
+	
+}
