1d58f37d0364132ef1439c384969fd04fcb3488f
Rock Hudson <rockhudsonmillas@gmail.com>
2024-10-17 11:36:02 -0300

    POO - Herança

--- Exercicios/src/o/o/heranca/Jogo.java
+++ Exercicios/src/o/o/heranca/Jogo.java
@@ -6,14 +6,28 @@ public class Jogo {
 
 		Jogador jogador1 = new Jogador();
 
-		jogador1.x = 20;
+		jogador1.x = 10;
 		jogador1.y = 10;
 
-		jogador1.andar(Direcao.NORTE);
-		jogador1.andar(Direcao.NORTE);
-		jogador1.andar(Direcao.NORTE);
-
-		System.out.println(jogador1.y);
+		
+		Jogador jogador2 = new Jogador();
+		
+		jogador2.x = 10;
+		jogador2.y = 10;
+		
+		
+		System.out.println(jogador1.vida);
+		System.out.println(jogador2.vida);
+		
+		jogador1.atacar(jogador2);
+		
+		System.out.println(jogador2.vida);
+			
+			
+			
+			
+		
+		
 
 	}
 
